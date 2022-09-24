61038f19bc6220946f5637113ac10da70a27bed1
Winfried Baumann <codewing@web.de>
2022-09-24 15:55:08 +0200

    more ui stuff

--- src/ui/main_window.rs
+++ src/ui/main_window.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 use eframe::egui::{Frame, TopBottomPanel};
 use eframe::egui::style::Margin;
+use eframe::emath::Align;
 use egui_dock::{NodeIndex, DockArea, DynamicTree, TabBuilder};
 use crate::AdventureProject;
 
@@ -8,7 +9,9 @@ use super::tab_example::{Editor};
 use super::scene_view::{SimpleSceneView};
 
 pub struct MainWindow {
-    tree: DynamicTree
+    tree: DynamicTree,
+    project: Rc<AdventureProject>,
+    show_rename_dialog: bool,
 }
 
 impl MainWindow {
@@ -43,23 +46,54 @@ impl MainWindow {
         let [_, _] = tree.split_below(a, 0.7, vec![tab4]);
         let [_, _] = tree.split_below(b, 0.5, vec![tab5]);
 
-        Self { tree }
+        Self { tree, project, show_rename_dialog: false }
     }
 
     pub fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         TopBottomPanel::top("top")
             .frame(Frame::none().inner_margin(Margin::same(2.0)))
             .show(ctx, |ui| {
-                if ui.button("Add Editor").clicked() {
-                    self.tree
-                        .push_to_focused_leaf(Box::new(Editor::new("New Text".into())));
-                }
+                ui.menu_button("File", |ui| {
+                    if ui.button("Open Project").clicked() {
+
+                    }
+
+                    if ui.button("Rename Project").clicked() {
+                        self.show_rename_dialog = true;
+                    }
+                });
+                ui.menu_button("View", |ui| {
+                    
+                });
             });
         DockArea::new(&mut self.tree).show(ctx, &mut egui_dock::DynamicTabViewer {});
+
+        if(self.show_rename_dialog) {
+            eframe::egui::Window::new("Rename Project")
+                .collapsible(false)
+                .resizable(false)
+                .anchor(eframe::egui::Align2::CENTER_CENTER, eframe::egui::Vec2::ZERO)
+                .show(ctx, |ui| {
+                    ui.vertical(|ui| {
+                        let mut title = self.project.title.to_string();
+                        ui.text_edit_singleline(&mut title);
+                    });
+                    ui.horizontal(|ui| {
+                        if ui.button("Cancel").clicked() {
+                            self.show_rename_dialog = false;
+                        }
+
+                        if ui.button("Rename").clicked() {
+                            self.show_rename_dialog = false;
+                        }
+                    });
+                });
+        }
+
     }
 }
 
-
+// self.tree.push_to_focused_leaf(Box::new(Editor::new("New Text".into())));
 
 struct TabViewer {}
 
--- src/ui/scene_view.rs
+++ src/ui/scene_view.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use std::rc::Rc;
 use eframe::egui::{Context, Ui, SidePanel, CentralPanel, Area, pos2, Frame, WidgetText, TextBuffer};
 use eframe::egui::style::Margin;
@@ -11,14 +12,6 @@ pub struct SimpleSceneView {
 }
 
 impl SimpleSceneView {
-
-    fn empty() -> Self {
-        Self{
-            scenes: vec![],
-            current_scene: None
-        }
-    }
-
     pub fn new(scenes: Vec<Rc<Scene>>) -> Self {
         let first_scene;
         if scenes.len() > 0 {
@@ -27,7 +20,7 @@ impl SimpleSceneView {
             first_scene = None;
         }
 
-        Self{
+        Self {
             scenes,
             current_scene: first_scene,
         }
@@ -36,28 +29,45 @@ impl SimpleSceneView {
 
 impl Tab for SimpleSceneView {
     fn ui(&mut self, ui: &mut Ui) {
-        Frame::none()
-            .inner_margin(Margin::same(2.0))
-            .show(ui, |ui| {
-                ui.horizontal(|ui| {
-                    ui.vertical(|ui| {
-                        for scene in self.scenes.iter() {
-                            if ui.button(scene.title.as_str()).clicked() {
-                                self.current_scene = Some(scene.clone());
-                            }
-                        }
-                    });
-                    ui.vertical(|ui| {
-                        match &self.current_scene {
-                            Some(elem) => ui.label(format!("Scene: {}", elem.title)),
-                            None => ui.label("Please select a scene.")
-                        };
-                    });
-                });
-            });
+        ui.vertical(|ui| {
+            self.draw_scenes_ui(ui);
+            ui.allocate_space(ui.available_size());
+        });
+        ui.separator();
+        ui.horizontal(|ui| {
+            if ui.button("Add Scene").clicked() {
+                let index = i32::try_from(self.scenes.len());
+                let mut scene_index = i32::MAX;
+                match index {
+                    Ok(val) => scene_index = val + 1,
+                    Err(_) => ()
+                }
+                self.scenes.push(Rc::new(Scene { index: scene_index, title: format!("Scene {}", self.scenes.len() + 1) }));
+            }
+        });
     }
 
     fn title(&mut self) -> WidgetText {
         "Scene View".into()
+    }
+}
+
+impl SimpleSceneView {
+    fn draw_scenes_ui(&mut self, ui: &mut Ui) {
+        ui.horizontal(|ui| {
+            ui.vertical(|ui| {
+                for scene in self.scenes.iter() {
+                    if ui.button(scene.title.as_str()).clicked() {
+                        self.current_scene = Some(scene.clone());
+                    }
+                }
+            });
+            ui.vertical(|ui| {
+                match &self.current_scene {
+                    Some(elem) => ui.label(format!("Scene: {}", elem.title)),
+                    None => ui.label("Please select a scene.")
+                };
+            });
+        });
     }
 }
